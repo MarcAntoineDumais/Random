88b9f63f9a460a647e52ff603d8c429b24b24a94
Marc-Antoine Dumais <mdumais@expedia.com>
2021-12-28 21:05:36 -0500

    tweak DDR sensitivity

--- ddr_pad/ddr_pad.ino
+++ ddr_pad/ddr_pad.ino
@@ -1,11 +1,11 @@
 #include <Keyboard.h>
 
 // Edit the keys to change which key each pin corresponds to
-int keys[4] = {KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_LEFT_ARROW, KEY_RIGHT_ARROW};
+int keys[4] = {KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW};
 int pins[4] = {A0, A1, A2, A3};
-bool debug = true;
-int lowThreshold = 300;
-int highThreshold = 800;
+bool debug = false;
+int lowThreshold[4] = {600, 700, 300, 700};
+int highThreshold[4] = {625, 725, 325, 725};
 
 int vals[4];
 bool pressed[4];
@@ -21,11 +21,11 @@ void setup() {
 void loop() {
   for (int i = 0; i < 4; i++) {
     vals[i] = readKey(pins[i]);
-    if (!pressed[i] && vals[i] > highThreshold) {
+    if (!pressed[i] && vals[i] > highThreshold[i]) {
       pressed[i] = true;
       Keyboard.press(keys[i]);
     }
-    if (pressed[i] && vals[i] < lowThreshold) {
+    if (pressed[i] && vals[i] < lowThreshold[i]) {
       pressed[i] = false;
       Keyboard.release(keys[i]);
     }
@@ -34,12 +34,12 @@ void loop() {
   if (debug) {
     printKeys();
   }
-  delay(10);
+  delay(2);
 }
 
 int readKey(int pin) {
   analogRead(pin);
-  delay(1);
+  //delay(1);
   return analogRead(pin);
 }
 
